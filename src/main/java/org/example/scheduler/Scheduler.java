b21fbfc81386d7bceb86dbaa6286e09407f1978e
Jorge H. Cue Cantu <jorge.cue@gmail>
2021-11-30 19:51:38 -0600

    Scheduler step2 write code to make tests pass

--- src/main/java/org/example/scheduler/Scheduler.java
+++ src/main/java/org/example/scheduler/Scheduler.java
@@ -1,19 +1,22 @@
 package org.example.scheduler;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.DelayQueue;
+import java.util.stream.Collectors;
 
 public class Scheduler {
     @SuppressWarnings("unused")
     private final DelayQueue<Command> queue = new DelayQueue<>();
 
     public void enqueue(Collection<? extends Command> commands) {
-        // WIP
+        queue.addAll(commands);
     }
 
     public Collection<CompletableFuture<Void>> launch() {
-        return Collections.emptyList();
+        Collection<? super Command> ready = new ArrayList<>();
+        queue.drainTo(ready);
+        return ready.stream().map(Runnable.class::cast).map(CompletableFuture::runAsync).collect(Collectors.toList());
     }
 }
